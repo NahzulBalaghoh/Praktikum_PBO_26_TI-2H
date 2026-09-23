31eab92091ac0f9b43125524f8704432a955f0eb
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-09-23 21:00:13 +0700

    P4_Percobaan6

---
+++ P4/id.ac.polinema.relasiclass.percobaan6/laptop.java
@@ -0,0 +1,12 @@
+public class laptop {
+    private String merk;
+
+    public laptop(String merk) {
+        this.merk = merk;
+    }
+
+    public void cetakDokumen(Printer printer, String namaFile) {
+        System.out.println(merk + " mengirim dokumen ke printer..." );
+        printer.cetak(namaFile);
+    }
+}
